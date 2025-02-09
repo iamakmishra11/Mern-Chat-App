b48719417db42952fb764a0340c9ca8118436e61
Anuj Kumar Mishra <144154519+iamakmishra11@users.noreply.github.com>
2025-02-10 02:20:44 +0530

    Update webContainer.js

--- frontend/src/config/webContainer.js
+++ frontend/src/config/webContainer.js
@@ -1,10 +1,10 @@
-import { WebContainer } from '@webcontainer/api';
+import { webcontainer } from '@webcontainer/api';
 
 let webContainerInstance = null;
 
 export const getWebContainer = async () => {
     if (webContainerInstance === null) {
-        webContainerInstance = await WebContainer.boot();
+        webContainerInstance = await webcontainer.boot();
     }
     return webContainerInstance;
 };
